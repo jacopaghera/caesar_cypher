a98df0cd6a3fd8969a47c89bb9f7df8ae181517f
jacopaghera <123456789+jacopaghera@gmail.com>
2024-07-13 10:42:41 +0200

    final commit, works with uppercase letters

--- caesar_cypher.rb
+++ caesar_cypher.rb
@@ -1,15 +1,28 @@
 
+require 'pry-byebug'
+
+class String
+  def is_upper?
+    self == self.upcase
+  end
+end
+
 def caesar_cipher(string, shift_factor)
   result = ""
   string.each_char do |char|
     if char.match?(/[a-zA-Z]/)
-      position = char.downcase.ord - 'a'.ord+1 + shift_factor
-      result += position.chr
+      position = char.downcase.ord + 1 + shift_factor
+      if char.is_upper? == true
+        result += position.chr.upcase
+      else
+        result += position.chr
+      end
     else
       result += char
     end
   end
   puts result
 end
+
 
 caesar_cipher("Ciao sono Jacopo!", 3)
